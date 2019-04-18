c532f5ec116c9184186aa68f0e7c5b461774c7fe
Itai Tenenbaum <taitulism@gmail.com>
2019-04-19 01:55:16 +0300

    Del old src folder

--- _src/run-shell-cmd.js
+++
@@ -1,7 +0,0 @@
-module.exports = function runShellCmd (...args) {
-    const [cmd, cmdArgs, opts, callback] = this.resolveArguments(...args);
-
-    opts.shell = true;
-
-    return this.runCmd(cmd, cmdArgs, opts, callback);
-};
--- _src/spawn-shell.js
+++
@@ -1,7 +0,0 @@
-module.exports = function runInShell (...args) {
-    const [cmd, cmdArgs, opts] = this.resolveArguments(...args);
-
-    opts.shell = true;
-
-    return this.spawnProcess(cmd, cmdArgs, opts);
-};
