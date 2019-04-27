70a0334325440d929e6959fcf24eca4d267301f2
Itai Tenenbaum <taitulism@gmail.com>
2019-04-28 02:15:31 +0300

    remove duplicode

--- src/private-methods/helpers.js
+++ src/private-methods/helpers.js
@@ -1,16 +1,6 @@
 const {existsSync} = require('fs');
-const getLogMsg = require('./get-log-msg');
 
 const helpers = {
-	validateCommand (rawCmd) {
-		const isValid = (typeof rawCmd === 'string') && rawCmd.length > 0;
-
-		if (isValid) return;
-
-		const errMsg = getLogMsg.emptyCmd();
-
-		throw new Error(errMsg);
-	},
 	isBadCmd (cmd, err) {
 		if (!err) return false;
 
