b96414ab775da489216abedaa443ebeb137ba066
Itai Tenenbaum <taitulism@gmail.com>
2019-08-18 23:57:50 +0300

    playground

--- playground/play.js
+++ playground/play.js
@@ -19,55 +19,29 @@ async function getFiles () {
 }
 
 (async () => {
-try {
-	// await execFile('echod hello', (err, out, warns) => {
-	// 	console.log('err', err);
-	// 	console.log('out', out);
-	// 	console.log('warns', warns);
-	// })
+	try {
+		// const cp = cwd.spawn('  ls ', ['../'], {stdio: 'pipe'})
+		const cp = cwd.runCmd('  ls ', ['./'], {stdio: 'pipe'})
 
-	// return;
+		cp.then((p) => {
+			console.log('p', p);
+		})
 
+		// cp.on('line', (line) => {
+		// 	console.log('line', line);
+		// })
 
-
-	// const cp = spawn('ls &&', ['echo hiiii', '&& echo bye'], {shell:true})
-	const cp = cwd.spawn('  ls ', ['./'])
-
-	cp.on('line', (lines) => {
-		console.log('LINE:', line);
-	})
-	// cp.stdout.on('data', (chunk) => {
-	// 	console.log('CHUNK:', chunk.toString());
-	// })
-	cp.on('close', (code) => {
-		console.log('closed', code);
-	})
-	cp.on('error', (err) => {
-		console.log('ERROR:\n', err);
-	})
-
-	// cp.kill()
-
-	console.log('file:', cp.spawnfile);
-	console.log('args:', cp.spawnargs);
-	console.log('');
-
-
-
-	setTimeout(() => {}, 5000);
-} catch (ex) {
-	console.log('EX:\n', ex);
-}
-
-
-
-	// const p = await getFiles().then((res) => {
-	// 	console.log(res);
-	// })
+		// cp.on('close', (code) => {
+		// 	console.log('code', code);
+		// })
 
 
 
 
+		setTimeout(() => {}, 5000);
+	} catch (ex) {
+		console.log('EXCEPTION:\n', ex);
+	}
 
 	setTimeout(() => {}, 3000);
 })()
